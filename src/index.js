fa2818c0077baa28e1694241412406d68cceb469
sky96111 <sky96111@outlook.com>
2024-07-10 00:27:37 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.skyme.club": dockerHub,
+  "quay.skyme.club": "https://quay.io",
+  "gcr.skyme.club": "https://gcr.io",
+  "k8s-gcr.skyme.club": "https://k8s.gcr.io",
+  "k8s.skyme.club": "https://registry.k8s.io",
+  "ghcr.skyme.club": "https://ghcr.io",
+  "cloudsmith.skyme.club": "https://docker.cloudsmith.io",
+  "ecr.skyme.club": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.skyme.club": dockerHub,
 };
 
 function routeByHosts(host) {
